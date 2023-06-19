33aad85f7f4117700f7dd8a4eaa20d5d5d545d22
Oskari Rautiainen <orautiainen@splunk.com>
2023-06-19 15:32:15 -0700

    Stop printing a stringified JSON

--- src/output.rs
+++ src/output.rs
@@ -7,7 +7,7 @@ use std::io::Write;
 pub fn write_output(output: Output) -> std::io::Result<()> {
     let file = File::create("report.json")?;
     let mut writer = BufWriter::new(file);
-    serde_json::to_writer(&mut writer, &serde_json::to_string(&output).unwrap())?;
+    serde_json::to_writer(&mut writer, &output)?;
     writer.flush().unwrap();
     Ok(())
 }
